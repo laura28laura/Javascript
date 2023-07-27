3048b1d6dd65c57c6c1b0a5299fd3513e7256454
laurence azghn <laurenceezghen013@gmail.com>
2023-07-27 09:12:21 +0300

    task5 done

---
+++ Task_3/script.js
@@ -0,0 +1,16 @@
+const num1="9";
+var num="3";
+let canpass=true;
+if (canpass==true)
+
+    if (num1=>num) {
+
+    
+    console.log ("hello");
+
+} else {
+    console.log ("bye");
+
+} else { 
+    console.log("try again")
+}
---
+++ task_4/script.js
@@ -0,0 +1,7 @@
+
+const numbers = [1, 2, 3, 4, 5];
+const fruits = ["apple", "banana", "orange"];
+const mixedArray = [1, "hello",]
+console.log (numbers)
+console.log (fruits)
+console.log (mixedArray)
